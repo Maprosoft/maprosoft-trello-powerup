775b3e1c7258690d3baaae17bcfb2cb4a94b8cc0
Dugald Morrow <dugald@maprosoft.com>
2017-01-15 08:50:26 +1100

    update

--- js/template.js
+++ js/template.js
@@ -5,6 +5,7 @@
 //
 //var retrieveSharedMapsUrl = 'https://www.maprosoft.com/app/shared?team=demo&getSharedMapNames=yes';
 //var cachedSharedMapNames = [];
+var cachedCardInfo = {};
 
 var getBadges = function(t) {
   return t.card('name')
@@ -54,41 +55,6 @@ var getBadges = function(t) {
   })
 };
 
-//var doGet = function(url) {
-//  var getPromise = new Promise(function(resolve, reject) {
-//    var request = new XMLHttpRequest();
-//    request.open('GET', url, true);
-//    request.onload = function() {
-//      try {
-//        if (request.status === 200) {
-//          var responseJson = JSON.parse(request.responseText);
-//          return resolve(responseJson);
-//        } else if (request.status === 404) {
-//          return reject(new i18nError.LocaleNotFound(targetLocale + " not found."));
-//        } else {
-//          return reject(new i18nError.Unknown("Unable to load locale, status: " + request.status));
-//        }
-//      } catch(ex) {
-//        return reject(new i18nError.Unknown(ex.message));
-//      }
-//    };
-//    request.send();
-//  });
-//  return getPromise;
-//};
-//
-//var formatNPSUrl = function(t, url){
-//  if(!/^https?:\/\/www\.nps\.gov\/[a-z]{4}\//.test(url)){
-//    return null;
-//  }
-//  var parkShort = /^https?:\/\/www\.nps\.gov\/([a-z]{4})\//.exec(url)[1];
-//  if (parkShort && parkMap[parkShort]){
-//    return parkMap[parkShort];
-//  } else{
-//    return null;
-//  }
-//};
-
 var boardButtonCallback = function(t){
   return t.popup({
     title: 'Popup List Example',
@@ -121,38 +87,49 @@ var boardButtonCallback = function(t){
   });
 };
 
-var cardButtonCallback = function(t) {
+var primeSharedMapInfo = function(t) {
   var popupItems = [];
   t.get('board', 'shared', 'cached-shared-map-info', null).then(function(data) {
     if (data) {
       var sharedMapInfo = data;
       if (sharedMapInfo && sharedMapInfo.mapNames) {
-        popupItems = Object.keys(sharedMapInfo.mapNames).map(function(index) {
-          var sharedMapName = sharedMapInfo.mapNames[index];
-          var teamKey = 'demo';
-          var encodedSharedMapName = encodeURIComponent(sharedMapName);
-          var sharedMapUrl = 'https://www.maprosoft.com/app/shared/' + teamKey + '/' + encodedSharedMapName;
-          return {
-            text: sharedMapName,
-            url: sharedMapUrl,
-            callback: function(t) {
-              return t.attach({
-                url: sharedMapUrl,
-                name: sharedMapName
-              })
-                  .then(function(){
-                    return t.closePopup();
-                  });
-            }
-          };
-        });
+        cachedCardInfo = sharedMapInfo;
       } else {
-        popupItems = [];
+        // ???
       }
     } else {
       // ???
     }
   });
+};
+
+var cardButtonCallback = function(t) {
+  primeSharedMapInfo(t);
+
+  var sharedMapInfo = cachedCardInfo;
+  if (sharedMapInfo && sharedMapInfo.mapNames) {
+    var popupItems = Object.keys(sharedMapInfo.mapNames).map(function(index) {
+      var sharedMapName = sharedMapInfo.mapNames[index];
+      var teamKey = 'demo';
+      var encodedSharedMapName = encodeURIComponent(sharedMapName);
+      var sharedMapUrl = 'https://www.maprosoft.com/app/shared/' + teamKey + '/' + encodedSharedMapName;
+      return {
+        text: sharedMapName,
+        url: sharedMapUrl,
+        callback: function(t) {
+          return t.attach({
+            url: sharedMapUrl,
+            name: sharedMapName
+          })
+              .then(function(){
+                return t.closePopup();
+              });
+        }
+      };
+    });
+  } else {
+    var popupItems = [];
+  }
   return t.popup({
     title: 'Select a Maprosoft map',
     items: popupItems,
@@ -203,6 +180,8 @@ var cardButtonCallbackOLD = function(t) {
 //doGet(retrieveSharedMapsUrl).then(function(data) {
 //  cachedSharedMapNames = data.mapNames;
 //});
+
+//primeSharedMapInfo(t);
 
 TrelloPowerUp.initialize({
   'attachment-sections': function(t, options) {
