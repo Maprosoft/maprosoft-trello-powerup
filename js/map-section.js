824512c530250e311864fee039eef9c179a7762a
Dugald Morrow <dugald@maprosoft.com>
2017-01-22 00:42:34 +1100

    update

--- js/map-section.js
+++ js/map-section.js
@@ -19,6 +19,20 @@ t.render(function () {
     var mapFrameElement = document.getElementById('map-frame');
     if (inOverlayMode) {
         mapFrameElement.src = mapUrl;
+
+        // close overlay if user clicks outside our content
+        document.addEventListener('click', function(e) {
+            if(e.target.tagName == 'BODY') {
+                t.closeOverlay().done();
+            }
+        });
+
+        // close overlay if user presses escape key
+        document.addEventListener('keyup', function(e) {
+            if(e.keyCode == 27) {
+                t.closeOverlay().done();
+            }
+        });
     } else {
         t.card('attachments')
         .get('attachments')
