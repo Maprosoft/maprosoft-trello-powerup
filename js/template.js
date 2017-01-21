3407983045b1308de7f094b31d49e0ca8e532515
Dugald Morrow <dugald@maprosoft.com>
2017-01-22 01:52:44 +1100

    update

--- js/template.js
+++ js/template.js
@@ -264,7 +264,7 @@ TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: MAPROSOFT_ICON_WITH_TEXT_COLOR,
-      text: '',
+      text: 'x',
       callback: boardButtonCallback
     }];
   },
