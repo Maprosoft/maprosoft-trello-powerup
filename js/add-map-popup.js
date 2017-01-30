9cd0cb1a76abdc50d41160e5a781802c0ddfb09c
Dugald Morrow <dugald@maprosoft.com>
2017-01-30 17:40:09 +1100

    update

--- js/add-map-popup.js
+++ js/add-map-popup.js
@@ -1,21 +1,27 @@
+var selectASharedMapChoiceText = 'Select a shared map';
+
 $(document).ready(function() {
     initialiseAddMapPopup();
 });
 
 var initialiseAddMapPopup = function() {
     var $mapSelectionContainer = $('#map-selection-container');
-    $mapSelectionContainer.removeClass('hidden');
     var $sharedMapSelectionButton = $('#sharedMapSelectionButton');
+    $sharedMapSelectionButton.innerText = selectASharedMapChoiceText;
     $sharedMapSelectionButton.click(handleSharedMapSelectionButton);
     var $actionLink = $('#action-a');
     $actionLink.click(handleSharedMapSelectionLink);
     var $actionLinks = $('.shared-map-choice');
     $actionLinks.click(handleSharedMapSelectionLink);
+    $mapSelectionContainer.removeClass('hidden');
 };
 
 var handleSharedMapSelectionLink = function(event) {
     event.preventDefault();
     var $sharedMapSelectionButton = $('#sharedMapSelectionButton');
+    //$sharedMapSelectionButton.text('hhhhhhhhhhhhhh');
+    var sharedMapOption = event.target;
+    var sharedMapName = sharedMapOption.innerText;
     $sharedMapSelectionButton.text('hhhhhhhhhhhhhh');
 };
 
